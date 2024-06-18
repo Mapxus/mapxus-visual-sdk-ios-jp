debd749cfe8dea00258ce34ae25190994acc32bd
guochenghao <chenghaoguo@mapxus.com>
2024-06-18 16:10:32 +0800

    # CHANGELOG
    ## v6.9.0 (2024-06-18)
    
    ### 📝 Changes
    
    * Synchronous version.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "6.8.0"
+let version = "6.9.0"
 
 let package = Package(
   name: "MapxusVisualSDK",
@@ -19,6 +19,6 @@ let package = Package(
     .binaryTarget(
       name: "MapxusVisualSDK",
       url: "https://nexus3.mapxus.co.jp/repository/ios-sdk/\(version)/mapxus-visual-sdk-ios-jp.zip",
-      checksum: "80b70d8840fb4abcd009838e79d096a8ef9096e9e1f58122f44197774d72e020")
+      checksum: "24b1d5131835f03fada27f88661cbef77ed43b05d8a194d03f708e347f706e6a")
   ]
 )
