d0263ce6601061e70ee987225cb290f61e1520b7
chenjiajie <jiajiechen@mapxus.com>
2025-03-25 11:52:58 +0800

    # CHANGELOG
    ## v7.4.0 (2025-03-21)
    
    ### 📝 Changes
    
    * Synchronous version.

--- MapxusVisualSDK-jp.podspec
+++ MapxusVisualSDK-jp.podspec
@@ -2,7 +2,7 @@
 Pod::Spec.new do |s|
 
 
-  version = '7.3.0'
+  version = '7.4.0'
 
   s.name         = 'MapxusVisualSDK-jp'
   s.version      = version
--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "7.3.0"
+let version = "7.4.0"
 
 let package = Package(
   name: "MapxusVisualSDK",
@@ -19,6 +19,6 @@ let package = Package(
     .binaryTarget(
       name: "MapxusVisualSDK",
       url: "https://nexus3.mapxus.co.jp/repository/ios-sdk/\(version)/mapxus-visual-sdk-ios-jp.zip",
-      checksum: "ec74fc9b5adf4185e2d03f25acdb8ee9acc0c70d6cc077b8c849b254784b78e1")
+      checksum: "6b79fbb6869a994f49e8b40614cd0849117532c8c38fae71698a1a964fc52fd5")
   ]
 )
